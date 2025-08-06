73f2bb8c1995e4a5a13ccefb5074b52e481c15b0
Jia Yi <s709161616@gmail.com>
2025-08-06 13:43:02 +0800

    feat: index.html 加入登入註冊區塊，並新增 currentPage 變數

---
+++ src/controls/formsControl.js
@@ -0,0 +1,5 @@
+const formsControl = () => {
+  return {};
+};
+
+export { formsControl };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Alpine from "alpinejs";
 import { bookListControl } from "./controls/bookListControl";
 import { searchControl } from "./controls/searchControl";
+import { formsControl } from "./controls/formsControl";
 import { booksStore } from "./stores/booksStore";
 import { uiStore } from "./stores/uiStore";
 
 Alpine.data("bookListControl", bookListControl);
 Alpine.data("searchControl", searchControl);
+Alpine.data("formsControl", formsControl);
 Alpine.store("booksApp", { ...booksStore, ...uiStore });
 window.Alpine = Alpine;
 
